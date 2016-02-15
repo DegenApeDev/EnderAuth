778433882f37403d179352c74cf2632f9c5834fb
Driesboy <coenendries3@gmail.com>
2016-02-15 20:47:12 +0100

    Update EnderAuth.php

--- src/EnderAuthTeam/EnderAuth/EnderAuth.php
+++ src/EnderAuthTeam/EnderAuth/EnderAuth.php
@@ -1,1 +1,12 @@
+<?php
 
+namespace EnderAuth;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+
+public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		
+	}
